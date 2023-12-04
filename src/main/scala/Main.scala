20fed6f0e63d6b242c2c4af3141c82a9ad35c0dc
jherve <julien.jev.herve@gmail.com>
2023-12-04 12:03:49 +0100

    Implement solution for day 1 part 1

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,16 @@
-@main def hello: Unit =
-  println("Hello world!")
-  println(msg)
+import scala.io.Source
 
-def msg = "I was compiled by Scala 3. :)"
+def day_01(lines: Iterator[String]) = {
+  lines
+    .map(_.filter(_.isDigit).map(_.toString.toInt))
+    .map(l => (l.head, l.last))
+    .foldLeft(0)((acc, value) => acc + value._1 * 10 + value._2)
+}
+
+@main def hello(day: Int, part: Int, inputFile: String): Unit =
+  val lines = Source.fromFile(inputFile).getLines()
+
+  val result = (day, part) match
+    case (1, 1) => day_01(lines)
+
+  println(result)
